ab9798c253584c7407fcfabc050cf73667019804
micmarsh <mike@marsh.pw>
2014-07-03 17:38:10 -0400

    rendering looking HOT

--- src/bitsplit_cli/display.clj
+++ src/bitsplit_cli/display.clj
@@ -9,7 +9,28 @@
             (for [[i info] (enumerate addresses)]
                 {(str i) info}))})
 
-(declare renderable->string)
+(defn percent->string [big-dec]
+    (-> big-dec
+        (* 100)
+        (str "% ")))
+
+(defn- info->string [[index [address percent]]]
+    (str "    "
+         "(" index ") "
+         address " " (percent->string percent)
+         \newline))
+
+(defn- render-pair->string [[index info]]
+    (apply str "(" index ") " 
+        (:address info) 
+        \newline
+        (map info->string 
+            (:to info))))
+
+(defn renderable->string [renderable]
+    (->> renderable
+        (map render-pair->string)
+        (apply str)))
 
 (defn splits->renderable [splits]
     (apply merge
