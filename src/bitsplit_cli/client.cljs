1006de5482496933bf34c60bc1a090028f5fb118
micmarsh <mike@marsh.pw>
2014-07-10 17:56:01 -0400

    complete other methods

--- src/bitsplit_cli/client.cljs
+++ src/bitsplit_cli/client.cljs
@@ -5,12 +5,15 @@
         (Queries addresses unspent-amounts unspent-channel
          Operations send-amounts! new-address!)]))
 
+(def fake-addresses (atom
+    ["dke98di398fdjr98feijr3oifsoij"
+     "kdi9d9ekdkjeufjeueudjudd"
+     "83dkm3mde030dk3kd3jdmjewkdi"]))
+(def findex (atom 0))
+
 (defrecord FakeClient [info]
     Queries
-    (addresses [this]
-        ["dke98di398fdjr98feijr3oifsoij"
-         "kdi9d9ekdkjeufjeueudjudd"
-         "83dkm3mde030dk3kd3jdmjewkdi"])
+    (addresses [this] @fake-addresses)
     (unspent-amounts [this]
         (vec 
             (map (fn [address amount] {"address" address "amount" amount})
@@ -18,15 +21,17 @@
     (unspent-channel [this]
         (let [amounts (unspent-amounts this)
               index (atom 0)
-              return (chan)]
+              return (chan)
+              inc-mod (comp #(mod % 3) inc)]
             (js/setInterval
                 (fn []
-                    (put! return (amounts @index))
-                    (swap! index inc)
-                    (swap! index #(mod % 3)))
+                    (put! return 
+                        (amounts (swap! index inc-mod))))
                 1000)
             return))
     Operations
-    (send-amounts! [this amounts])
-    (new-address! [this]))
+    (send-amounts! [this amounts] (println "sending" amounts))
+    (new-address! [this]
+        (swap! fake-addresses conj 
+            (str "newest-address" (swap! findex inc)))))
 
