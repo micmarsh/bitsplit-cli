363fed1ef0546af5b847a4f51ba503883a010252
micmarsh <mike@marsh.pw>
2014-09-20 17:55:32 -0400

    dupes are no good

--- src/bitsplit_cli/app/options.cljs
+++ src/bitsplit_cli/app/options.cljs
@@ -26,14 +26,23 @@
     (when-not v
       (throw (js/Error. (str "Unrecognized option: " k))))))
 
+(defn error-on-dupes [seq]
+  (doseq [seen [(atom #{})]
+          item (take-nth 2 seq)]
+    (if (contains? @seen item)
+      (throw (js/Error. (str "Duplicate option: " item)))
+      (swap! seen conj item)))
+  seq)
+
 (def key-transforms
-  {#{"-n" "--network"} "network"
-   #{"-i" "--interval"} "interval"
-   #{"-v" "--verbose"} "verbose"} )
+  {#{"-n" "--network"} :network
+   #{"-i" "--interval"} :interval
+   #{"-v" "--verbose"} :verbose})
 
 (def args->options
   (comp
    verify-options
    (partial transform-keys key-transforms)
    seq->map
+   error-on-dupes
    rest))
